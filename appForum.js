6caaaff2c1d33bd87e434e8ef949a954e27dcd4e
Mariusz Seget <97680265+Isztof@users.noreply.github.com>
2022-02-05 11:09:04 +0100

    forgot to push the updated js file

--- appForum.js
+++ appForum.js
@@ -1,10 +1,19 @@
 console.log("Hello Word")
 //Global Selectors
 const  main = document.querySelector('main');
+const postB = document.querySelector("#postB");
+var postTitleInput = document.querySelector("#title");
+var postDescriptionInput = document.querySelector("#comment");
+//var titleHtml = document.querySelector(".noteTitle");
+
+// var postTitle = document.querySelector("#t").value;
+
 
 // Adding a new post
 function addingPost(){
-    let frame =  document.createElement("div");
+  let postTitle = postTitleInput.value;
+  let postDescription = postDescriptionInput.value; 
+  let frame =  document.createElement("div");
     frame.className = "frame";
     frame.innerHTML = ` 
     <div class="authorDateTag">
@@ -12,24 +21,30 @@ function addingPost(){
     <!-- <div class="picture" ></div> -->
      <div class="authorTag">John Doe</div>
      <div class="dateTag">27.01.2021 21:49</div>
-     <h4 class="noteTitle" >I will be replaced by a variable with the title</h4> 
+     <h4 class="noteTitle" > ${postTitle}</h4> 
    </div>
   
-     <div class="noteDescription"> "Description" - going to be replaced by the description from the input field 
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-       dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-       proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-     </div>
+     <div class="noteDescription"> ${postDescription} </div>
     <div class="postBtns">
        <button  class="commentBtn"> Comment</button> 
        
        <button class="repButton">Report</button> 
    </div>
     `
-    main.appendChild(frame);
+    console.log(postTitle);
+   // var textTitle = document.createTextNode(postTitle);
+   // frame.appendChild(textTitle);
+     if (postTitle === ''){
+       alert("Your Post! has to have a Title");
+     } else {
+        main.appendChild(frame);
+        //titleHtml.innerHTML = postTitle;
+      
+     }
+     
+    
 }
 
-const postB = document.querySelector("#postB");
+
 
 postB.addEventListener('click', addingPost);
