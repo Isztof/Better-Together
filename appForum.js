9b9a9e0af479a0453ff013caa1d988ace94b3937
Mariusz Seget <97680265+Isztof@users.noreply.github.com>
2022-02-11 16:49:52 +0100

    Add files via upload

--- appForum.js
+++ appForum.js
@@ -4,12 +4,10 @@ const  main = document.querySelector('main');
 const postB = document.querySelector("#postB");
 var postTitleInput = document.querySelector("#title");
 var postDescriptionInput = document.querySelector("#comment");
-//var titleHtml = document.querySelector(".noteTitle");
-
-// var postTitle = document.querySelector("#t").value;
+const commentB = document.querySelector(".commentBtn")
 
 
-// Adding a new post
+// Adding a new post function 
 function addingPost(){
   let postTitle = postTitleInput.value;
   let postDescription = postDescriptionInput.value; 
@@ -32,19 +30,91 @@ function addingPost(){
    </div>
     `
     console.log(postTitle);
-   // var textTitle = document.createTextNode(postTitle);
-   // frame.appendChild(textTitle);
+
      if (postTitle === ''){
-       alert("Your Post! has to have a Title");
-     } else {
+       alert("Your Post has to have a Title!");
+     } else if (postDescription ===''){
+       alert("Your post has to have a Description!")
+     }
+      else {
         main.appendChild(frame);
         //titleHtml.innerHTML = postTitle;
-      
      }
-     
-    
+     // Comments section 
+
+    // comment panel
+     /*
+    comPanel = document.createElement("div");
+    comPanel.className = "panel";
+    frame.appendChild(comPanel);
+    */
 }
 
 
+// appending the adding new post function to a button 
 
 postB.addEventListener('click', addingPost);
+
+// openeing the comment panel  
+const frame = document.querySelector(".frame");
+
+comPanel = document.createElement("div");
+comPanel.className = "panel";
+frame.appendChild(comPanel);
+comPanel.innerHTML = `
+<div class="comments"> </div>
+<span class="commentLine">  <textarea class="areaT" placeholder="Type in your comment"></textarea><button type="submit" class="btn btn-info btn-sm shadow-none text-left" data-bs-toggle="button" autocomplete="off">comment</button> </span>
+`
+
+function showPanel(){
+ // alert("I work");
+  comPanel.className = "panel";
+  //comPanel.classList.toggle("active");
+  if (comPanel.style.display === "block") {
+    comPanel.style.display = "none";
+  } else {
+    comPanel.style.display = "block";
+  }
+
+}
+
+commentB.addEventListener('click', showPanel);
+
+// adding new comments to the comment panel 
+
+const addComB = document.querySelector(".commentLine button");
+var comInput = document.querySelector(".areaT");
+var commentsSection = document.querySelector(".comments");
+
+function addComment(){
+  //alert("I work!");
+  var comment = document.createElement("div");
+  comment.className = "comment";
+  let inputValue = comInput.value;
+  var t = document.createTextNode(inputValue);
+ // comment.appendChild(t);
+  comment.innerHTML = `
+    <span class="wide"><img class="picture" src="imgs/50x50picture.png"> <span class="commentAuthor">Mike Miller </span> </span>
+     <div class="commentText"> ${inputValue} </div> 
+
+  ` 
+  /*
+  let comText = document.querySelector(".commentText");
+  comText.appendChild(t);
+  */
+  commentsSection.appendChild(comment);
+  
+
+
+}
+
+addComB.addEventListener('click',addComment);
+
+// comment input resizer 
+
+const textArea = document.querySelector(".areaT");
+textArea.addEventListener("keyup", e =>{
+  let scHeight = e.target.scrollHeight;
+  console.log(scHeight);
+  textArea.style.height = `${scHeight}px`
+});
