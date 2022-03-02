83d33ba42134d172990837ae71d8fcf5a4faaec8
Isztof <mariuszseget77@gmail.com>
2022-03-02 22:08:42 +0100

    Apllying changes to get post

--- netlify/functions/get-posts.js
+++ netlify/functions/get-posts.js
@@ -1,25 +1,20 @@
-require('dotenv').config();
-const {
-    DATABASE_URL,
-    SUPABASE_SERVICE_API_KEY
-} = process.env;
-const { createClient } = require('@supabase/supabase-js');
+require("dotenv").config();
+const { DATABASE_URL, SUPABASE_SERVICE_API_KEY } = process.env;
+const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
-exports.handler = async event => {
-    const { data, error } = await supabase
-        .from('post')
-        .select('*');
+exports.handler = async (event) => {
+  const { data, error } = await supabase.from("post").select("*");
 
-    if (error) {
-        return {
-            statuscode: 500,
-            body: error,
-        };
-    }
-
+  if (error) {
     return {
-        statusCode: 200,
-        body: data,
+      statuscode: 500,
+      body: JSON.stringify(error),
     };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 };
