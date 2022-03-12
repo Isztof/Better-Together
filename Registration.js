1955156cc3d99c0310ee80ab979cf945d70fcb33
Isztof <mariuszseget77@gmail.com>
2022-03-12 12:14:21 +0100

    Attepmt to fix fetch 6

--- Registration.js
+++ Registration.js
@@ -115,13 +115,13 @@ registerButton.addEventListener("click", function () {
       body: userObject,
     })
       .then((response) => response.json())
-      .then(comsole.log(response));
+      .then(console.log("I don't get what's the point of this log"));
   } catch (error) {
     fetch("/.netlify/functions/user_profile", {
       method: "POST",
       body: userObject,
     })
       .then((response) => response.json())
-      .then(comsole.log(response));
+      .then(console.log("I don't get what's the point of this log"));
   }
 });
