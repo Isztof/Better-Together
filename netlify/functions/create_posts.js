e7ee77d9e56f6d4f4c81f6752fad734ff5de5220
DumanZhao <97738520+DumanZhao@users.noreply.github.com>
2022-03-14 19:25:12 +0100

    Update create_posts.js

--- netlify/functions/create_posts.js
+++ netlify/functions/create_posts.js
@@ -1,10 +1,10 @@
 // Grab our credentials from a .env file or environment variables
 require("dotenv").config();
-const { DATABASE_URL, SUPABASE_SERVICE_API_KEY } = process.env;
+const { SUPABASE_URL, SUPABASE_SERVICE_KEY } = process.env;
 
 // Connect to our database
 const { createClient } = require("@supabase/supabase-js");
-const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Our standard serverless handler function
 exports.handler = async (event) => {
