dd197925e38b19bce6e3690af51a378cb7e872cd
Isztof <mariuszseget77@gmail.com>
2022-03-14 19:17:06 +0100

    Correcting file name

---
+++ netlify/functions/create_posts.js
@@ -0,0 +1,36 @@
+// Grab our credentials from a .env file or environment variables
+require("dotenv").config();
+const { DATABASE_URL, SUPABASE_SERVICE_API_KEY } = process.env;
+
+// Connect to our database
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+
+// Our standard serverless handler function
+exports.handler = async (event) => {
+  // Insert a row
+  const { data, error } = await supabase
+    .from("posts")
+    .insert([
+      {
+        id: "2",
+        created_at: "17:10",
+        title: "Hello",
+        content: "This is a test",
+        modified_at: "",
+        user_id: "DZ",
+      },
+    ]);
+
+  if (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
