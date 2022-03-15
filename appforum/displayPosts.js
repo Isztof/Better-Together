7a2eade55c48c5f56f44b4285bab35d7766606cc
Isztof <mariuszseget77@gmail.com>
2022-03-15 23:16:34 +0100

    lets see

---
+++ appforum/displayPosts.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { SUPABASE_URL, SUPABASE_KEY } = process.env;
+const { createClient } = require("@supabase/supabase-js");
+const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function loadData() {
+  const { data, error } = await _supabase.from("posts").select();
+
+  console.log(data);
+  console.log(error);
+}
+
+loadData();
+
+async function loadData() {
+  const { data, error } = await _supabase.from("posts").select();
+  console.log(data);
+  /*
+  if (!error) {
+    //loop display data here
+    data.forEach(function (item) {
+      contents += `<div> ${item.title} - ${item.tag}</div>`;
+    });
+  }
+  */
+}
