b46251b392ab79d875e84e05d912488579df7f4e
Isztof <mariuszseget77@gmail.com>
2022-03-15 19:50:43 +0100

    Correcting fetch

--- appforum/indexforums.js
+++ appforum/indexforums.js
@@ -15,7 +15,7 @@ function addingPost() {
   console.log(array);
   // fetch the serverless function to pass the array to the database
   fetch(
-    "https://bettter-together-net.netlify.app/gitnetlify/functions/create_posts.js",
+    "https://bettter-together-net.netlify.app/netlify/functions/create_posts.js",
     {
       method: "POST",
       body: JSON.stringify(array),
