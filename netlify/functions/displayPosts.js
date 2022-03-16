c4ed6a830ca02ee393ca238d6036b5aa21d1cae1
Isztof <mariuszseget77@gmail.com>
2022-03-16 17:38:25 +0100

    Upload

--- netlify/functions/displayPosts.js
+++ netlify/functions/displayPosts.js
@@ -1,8 +1,5 @@
-//require("dotenv").config();
-//const { SUPABASE_URL, SUPABASE_KEY } = process.env;
-const SUPABASE_URL = "https://kebgtqeqyjbmfuyrzfzp.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODE3MzAyLCJleHAiOjE5NTkzOTMzMDJ9.Y2TDmG3yP93bfnczwaixjGjQO6wCAeXWU46lpS_MQEQ";
+require("dotenv").config();
+const { SUPABASE_URL, SUPABASE_KEY } = process.env;
 const { createClient } = require("@supabase/supabase-js");
 const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,7 +7,7 @@ exports.handler = async (event) => {
   async function loadData() {
     const { data, error } = await _supabase.from("posts").select();
 
-    console.log(data);
+    console.log(JSON.parse(data));
     if (error) {
       console.log(error);
     }
