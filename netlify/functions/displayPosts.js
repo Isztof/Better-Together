90c126049979c60ec20ddca9aa51c8a9d04a3bac
Isztof <mariuszseget77@gmail.com>
2022-03-16 12:06:28 +0100

    Upload

--- netlify/functions/displayPosts.js
+++ netlify/functions/displayPosts.js
@@ -2,20 +2,19 @@ require("dotenv").config();
 const { SUPABASE_URL, SUPABASE_KEY } = process.env;
 const { createClient } = require("@supabase/supabase-js");
 const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+try {
+  async function loadData() {
+    const { data, error } = await _supabase.from("posts").select();
 
-async function loadData() {
-  const { data, error } = await _supabase.from("posts").select();
-  try {
     console.log(data);
     if (error) {
       console.log(error);
     }
-  } catch (error) {
-    console.error(error);
   }
+  loadData();
+} catch (error) {
+  console.error(error);
 }
-
-loadData();
 /*
 async function loadData() {
   const { data, error } = await _supabase.from("posts").select();
