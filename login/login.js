f52187f0919d6160aaa7a4e49e25684ac8eda169
Isztof <mariuszseget77@gmail.com>
2022-03-17 22:36:45 +0100

    upload

--- login/login.js
+++ login/login.js
@@ -1,20 +1,24 @@
-fetch("../.netlify/functions/getUserAccounts")
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json);
-    json.forEach((item) => {
-      let userName = item.email;
-      let password = item.password;
-      let userNameInputBox = document.querySelector("#exampleInputEmail1");
-      let passwordInputBox = document.querySelector("#exampleInputPassword1");
-      console.log(userName == userNameInputBox.value);
-      if (
-        userName == userNameInputBox.value &&
-        password == passwordInputBox.value
-      ) {
-        window.location = "https://bettter-together-net.netlify.app/index.html";
-      } else {
-        alert("Username or password is invalid");
-      }
+const submitButton = document.querySelector("#post");
+submitButton.addEventListener("click", function () {
+  fetch("../.netlify/functions/getUserAccounts")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      json.forEach((item) => {
+        let userName = item.email;
+        let password = item.password;
+        let userNameInputBox = document.querySelector("#exampleInputEmail1");
+        let passwordInputBox = document.querySelector("#exampleInputPassword1");
+        console.log(userName == userNameInputBox.value);
+        if (
+          userName == userNameInputBox.value &&
+          password == passwordInputBox.value
+        ) {
+          window.location =
+            "https://bettter-together-net.netlify.app/index.html";
+        } else {
+          alert("Username or password is invalid");
+        }
+      });
     });
-  });
+});
