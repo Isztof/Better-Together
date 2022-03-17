0c21d10270fc19ec891935a2e088168c7e79cb0f
Isztof <mariuszseget77@gmail.com>
2022-03-17 22:48:14 +0100

    upload

--- login/login.js
+++ login/login.js
@@ -1,4 +1,6 @@
+/*
 const submitButton = document.querySelector("#post");
+console.log(submitButton);
 submitButton.addEventListener("click", function () {
   fetch("../.netlify/functions/getUserAccounts")
     .then((response) => response.json())
@@ -22,3 +24,28 @@ submitButton.addEventListener("click", function () {
       });
     });
 });
+*/
+
+async function login() {
+  fetch("../.netlify/functions/getUserAccounts")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      json.forEach((item) => {
+        let userName = item.email;
+        let password = item.password;
+        let userNameInputBox = document.querySelector("#exampleInputEmail1");
+        let passwordInputBox = document.querySelector("#exampleInputPassword1");
+        console.log(userName == userNameInputBox.value);
+        if (
+          userName == userNameInputBox.value &&
+          password == passwordInputBox.value
+        ) {
+          window.location =
+            "https://bettter-together-net.netlify.app/index.html";
+        } else {
+          alert("Username or password is invalid");
+        }
+      });
+    });
+}
