46c9ec4498c138408e118c557aa6736f2266bd73
DumanZhao <97738520+DumanZhao@users.noreply.github.com>
2022-03-18 22:07:42 +0100

    Update login.js

--- login/login.js
+++ login/login.js
@@ -67,7 +67,7 @@ let button = form.submit.addEventListener("click", (e) => {
         headers: {
             Accept: "application/json, text/plain, */*",
             "Content-Type": "application/json",
-        },
+       /* },
         body: JSON.stringify({
             email: form.email.value,
             password: form.password.value,
@@ -79,12 +79,12 @@ let button = form.submit.addEventListener("click", (e) => {
             // code here //
             if (data.error) {
                 alert("Error Password or Username"); /*displays error message*/
-            } else {
+           /* } else {
                 window.open(
                     "index.html"
-                ); /*opens the target page while Id & password matches*/
+                ); /* opens the target page while Id & password matches*/
             }
-        })
+       /* })
         .catch((err) => {
             console.log(err);
         });
