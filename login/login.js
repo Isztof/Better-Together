b14d08fcda0891b376b8d83332126ecab2d8fa94
DumanZhao <dumanzhao@gmail.com>
2022-03-22 21:41:06 +0100

    updates

--- login/login.js
+++ login/login.js
@@ -79,8 +79,8 @@ let button = form.submit.addEventListener("click", (e) => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data)
-      if (data.display_name == userNameInputBox.value &&
-        data.password == passwordInputBox.value) {
+      if (data.display_name !== userNameInputBox.value &&
+        data.password !== passwordInputBox.value) {
         window.open('/index.html');  /*opens the target page while Id & password matches*/
       } else {
         alert("Error Password or Username");/*displays error message*/
