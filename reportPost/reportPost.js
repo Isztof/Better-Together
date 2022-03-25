7e845e87c457e9f653e7a16ed7bb379e1e8a59c7
DumanZhao <dumanzhao@gmail.com>
2022-03-25 20:20:55 +0100

    updates

--- reportPost/reportPost.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(() => {
-    $("form").submit(function () {
-        if ($('input:checkbox').filter(':checked').length < 1) {
-            alert("Check at least one checkbox!");
-            return false;
-        }
-    });
-});
