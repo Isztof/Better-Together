5741eac6dd3caf49a6ee507774175f2b4752945f
Isztof <mariuszseget77@gmail.com>
2022-03-25 08:06:28 +0100

    restricting posting to logged in users

--- appforum/fetchPosts.js
+++ appforum/fetchPosts.js
@@ -1,43 +1,18 @@
-/*
-//local storage transfer from log in
-// transfers sessionStorage from one tab to another
-var sessionStorage_transfer = function (event) {
-  if (!event) {
-    event = window.event;
-  } // ie suq
-  if (!event.newValue) return; // do nothing if no value to work with
-  if (event.key == "getSessionStorage") {
-    // another tab asked for the sessionStorage -> send it
-    localStorage.setItem("sessionStorage", JSON.stringify(sessionStorage));
-    // the other tab should now have it, so we're done with it.
-    localStorage.removeItem("sessionStorage"); // <- could do short timeout as well.
-  } else if (event.key == "sessionStorage" && !sessionStorage.length) {
-    // another tab sent data <- get it
-    var data = JSON.parse(event.newValue);
-    for (var key in data) {
-      sessionStorage.setItem(key, data[key]);
-    }
+var id = 1; // post id
+
+// Redirection to edit post UI
+const takeToEditPostButton = document.querySelector("#postQ");
+takeToEditPostButton.addEventListener("click", function () {
+  if (localStorage.length) {
+    takeToEditPost();
+  } else {
+    alert("You have to log in first to post a new question");
   }
-};
+});
 
-// listen for changes to localStorage
-if (window.addEventListener) {
-  window.addEventListener("storage", sessionStorage_transfer, false);
-} else {
-  window.attachEvent("onstorage", sessionStorage_transfer);
+function takeToEditPost() {
+  window.location.href = "indexUserInputBox.html";
 }
-
-// Ask other tabs for session storage (this is ONLY to trigger event)
-if (!sessionStorage.length) {
-  localStorage.setItem("getSessionStorage", "foobar");
-  localStorage.removeItem("getSessionStorage", "foobar");
-}
-
-console.log(sessionStorage.getItem("getSessionStorage"));
-// End of getting the local storage from log in */
-console.log(localStorage.getItem("getSessionStorage"));
-const userName2 = localStorage.getItem("getSessionStorage");
-var id = 1;
 
 // display comments saved in the database
 async function fetchPostsWithComments() {
